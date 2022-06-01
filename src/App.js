9a701bbfb24c9ff904a4fcbea020e16e77436b23
Bliqui <ronlyred@gmail.com>
2022-06-02 00:39:35 +0200

    Add one main form to the project | It is a main part of the task, it has its own local state for managing user's choice and displaying revelant content

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import './App.css';
+import {useState} from "react";
+import {Soup} from "./components/Soup/Soup";
+import {Sandwich} from "./components/Sandwich/Sandwich";
+import {Pizza} from "./components/Pizza/Pizza";
 
 function App() {
+    const [dishType, setDishType] = useState('');
+
   return (
     <div className="App">
-        <form onSubmit={() => console.log("Hello, I'am form")}>
+        <form onSubmit={(e) => {
+            e.preventDefault()
+            console.log(dishType)
+        }}>
             <label>
-                Dish name:
+                <p>Dish name:</p>
                 <input type="text"/>
             </label>
+            <label>
+                <p>Preparation time:</p>
+                <input type="time"/>
+            </label>
+            <label>
+                <p>Dish type:</p>
+                <select name="dishTypes" onChange={(e) => setDishType(e.target.value)} id="1">
+                    <option value="pizza">Pizza</option>
+                    <option value="soup">Soup</option>
+                    <option value="sandwich">Sandwich</option>
+                </select>
+            </label>
+            {dishType === 'pizza' ? <Pizza/> : null}
+            {dishType === 'soup' ? <Soup/> : null}
+            {dishType === 'sandwich' ? <Sandwich/> : null}
+            <button>Submit</button>
         </form>
     </div>
   );

d3b9e83478a69f76065e3f1f414872cf9bce8991
Bliqui <ronlyred@gmail.com>
2022-06-02 02:19:36 +0200

    Provide several visual changes

--- src/App.js
+++ src/App.js
@@ -1,41 +1,46 @@
-import './App.css';
+import './App.scss'
 import {useState} from "react";
 import {Soup} from "./components/Soup/Soup";
 import {Sandwich} from "./components/Sandwich/Sandwich";
 import {Pizza} from "./components/Pizza/Pizza";
+import './assets/styles/index.scss'
 
 function App() {
     const [dishType, setDishType] = useState('');
 
-  return (
-    <div className="App">
-        <form onSubmit={(e) => {
-            e.preventDefault()
-            console.log(dishType)
-        }}>
-            <label>
-                <p>Dish name:</p>
-                <input type="text"/>
-            </label>
-            <label>
-                <p>Preparation time:</p>
-                <input type="time"/>
-            </label>
-            <label>
-                <p>Dish type:</p>
-                <select name="dishTypes" onChange={(e) => setDishType(e.target.value)} id="1">
-                    <option value="pizza">Pizza</option>
-                    <option value="soup">Soup</option>
-                    <option value="sandwich">Sandwich</option>
-                </select>
-            </label>
-            {dishType === 'pizza' ? <Pizza/> : null}
-            {dishType === 'soup' ? <Soup/> : null}
-            {dishType === 'sandwich' ? <Sandwich/> : null}
-            <button>Submit</button>
-        </form>
-    </div>
-  );
+    return (
+        <div className={'hexoceanMenu'}>
+            <div className={'hexoceanWrapper'}>
+                <form className={'hexoceanForm'} onSubmit={(e) => {
+                    e.preventDefault()
+                    console.log(dishType)
+                }}>
+                    <label className={'formLabel'}>
+                        <p className={'labelTitle'}>Dish name:</p>
+                        <input className={'normalInput'} type="text"/>
+                    </label>
+                    <label className={'formLabel'}>
+                        <p className={'labelTitle'}>Preparation time:</p>
+                        <input type="time"/>
+                    </label>
+                    <label className={'formLabel'}>
+                        <p className={'labelTitle'}>Dish type:</p>
+                        <select name="dishTypes" onChange={(e) => setDishType(e.target.value)} id="1">
+                            <option value="pizza">Pizza</option>
+                            <option value="soup">Soup</option>
+                            <option value="sandwich">Sandwich</option>
+                        </select>
+                    </label>
+                    {dishType === 'pizza' ? <Pizza/> : null}
+                    {dishType === 'soup' ? <Soup/> : null}
+                    {dishType === 'sandwich' ? <Sandwich/> : null}
+                    <div className={'buttonWrapper'}>
+                        <button className={'submitBtn'}>Submit</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
 }
 
 export default App;
